773e8a8d945fe461959320f4611faa611e70aea5
Faid Terence JABO <faidterence@outlook.com>
2023-03-17 18:21:50 +0200

    Testing

--- src/tests/blogs.test.js
+++ src/tests/blogs.test.js
@@ -1,26 +1,79 @@
-import request from 'supertest';
-import app from '../server.js';
-import Blog from '../models/Blog.js';
+import app from "../server.js"
+import mongoose from "mongoose";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import Blog from "../models/Blog.js";
+const should = chai.should();
 
+chai.use(chaiHttp);
 
-
-describe('Testing the Blog Controller', () => {
-    describe('Given the blog exists', () => {
-        it('it should return the blog', async() => {
-            const response = await request(app).get(`/api/v1/blogs/640dc171805368ffa5aedf6e`);
-            try {
-                expect(response.status).toBe(200);
-
-                
-            } catch (error) {
-                
-                expect(response.status).toBe(404);
-            }
+describe("CRUD Test", () => {
+    describe("METHODS ON BLOGS", ()=> {
+        it("Should return all blogs on GET", (done) => {
+            chai.
+            request(app)
+            .get('/api/v1/blogs')
+            .end((err,res) => {
+                if (err) done(err)
+                else {
+                    res.should.have.status(200)
+                    done()
+                }
+            })
         });
-    });
-    describe('Given the blog does not exist', () =>
-    {
-        const blogId= '640dc171805368ffa5aedf6r';
-        await(app).post(`api/v1/blogs{blogid}`).expect(404);
-    });
-});
+        it("Should create a new POST", (done) => {
+            chai.
+            request(app)
+            .post('/api/v1/blogs')
+            .send({
+                title: "Arificial Intelligence Test Integration",
+                body: "At its simplest form, artificial intelligence is a field, which combines computer science and robust datasets, to enable problem-solving.",
+                icon: "fa fa-robot"
+            })
+            .end((err,res) => {
+                if (err) done(err)
+                else {
+                    res.should.have.status(500);
+                    done()
+                }
+            })
+        });
+        it("Should return a specific POST ", (done) => {
+            chai.
+            request(app)
+            .get('/api/v1/blogs/6410e5de5493297eb48ab327')
+            .end((err,res) => {
+                if(err) done(err)
+                else{
+                    res.should.have.status(200)
+                    done()
+                }
+            })
+        });
+        it("Should Update a specific POST", (done) => {
+            chai.
+            request(app)
+            .put('/api/v1/blogs/6410e1e85493297eb48ab322')
+            .end((err,res) => {
+                if(err) done(err)
+                else {
+                    res.should.have.status(401);
+                    done()
+                }
+            })
+
+        });
+        it("Should Delete a Specific POST", (done) => {
+            chai.
+            request(app)
+            .delete('/api/v1/blogs/6410e1e85493297eb48ab322')
+            .end((err, res) => {
+                if(err) done(err)
+                else {
+                    res.should.have.status(401);
+                    done()
+                }
+            })
+        })
+    })
+})
